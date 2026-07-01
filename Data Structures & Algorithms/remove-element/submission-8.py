cc03129617d5421ea521115c349db666ffdbc900
Shuo <62603258+ShuoCode@users.noreply.github.com>
2026-07-01 16:48:54 +0100

    Add: remove-element - submission-8

---
+++ Data Structures & Algorithms/remove-element/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # because there are two types of elements, one equals val, one not
+        # so we need two pointers for each of them
+        # one starts from the beginning of the array
+        # only goes forward when this one is the element we are requiring
+        # the other one starts from the end of the array
+        # goes back when it is placed a not want element
+        # in this way, it doesn't need to go through the whole array
+        k = 0
+        n = len(nums)
+        while k < n:#
+            if nums[k] == val:
+                nums[k] = nums[n-1]
+                n -= 1
+                continue
+            k += 1
+        return k
